9f3ce0bbdb1391aa742c3de5f4dbb3b7c7d01045
ohaponiuk <64164850+ohaponiuk@users.noreply.github.com>
2026-02-24 12:12:32 +0000

    Style fixes

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 00:18:10 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/02/24 01:28:18 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/02/24 11:24:45 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
-	char	*s_cast;
 	char	c_cast;
 
 	i = ft_strlen(s);
-	s_cast = (char *)s;
 	c_cast = (char)c;
 	while (1)
 	{
-		if (s_cast[i] == c_cast)
-			return (&s_cast[i]);
+		if (s[i] == c_cast)
+			return ((char *)&s[i]);
 		if (i == 0)
 			break ;
 		i--;
