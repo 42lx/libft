81c6db405861fb8f4bbdff48c7e09b006ba6dc2d
ohaponiuk <64164850+ohaponiuk@users.noreply.github.com>
2026-02-24 18:12:16 +0000

    Format strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 11:13:00 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/02/24 13:11:22 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/02/24 18:12:02 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,9 +16,9 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
 	if (s2[0] == '\0')
 		return ((char *)&s1[0]);
+	i = 0;
 	while (i < n && s1[i])
 	{
 		j = 0;
