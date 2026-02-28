7bfd9297a3cf459c0cc2548e0d5229d5fe3c72b6
ohaponiuk <64164850+ohaponiuk@users.noreply.github.com>
2026-02-28 15:11:19 +0000

    Add itoa

---
+++ ft_itoa.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 13:22:06 by ohaponiu          #+#    #+#             */
+/*   Updated: 2026/02/28 15:08:39 by ohaponiu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	ft_abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
+
+static size_t	ft_intlen(int n)
+{
+	size_t	len;
+
+	len = 1 + (n < 0);
+	n = ft_abs(n);
+	while (n > 9)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	ft_itoa_rec(int n, char *res, size_t len, size_t i)
+{
+	if (n > 9)
+		ft_itoa_rec(n / 10, res, len, i + 1);
+	res[(len - 1) - i] = n % 10 + '0';
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	size_t	len;
+
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	len = ft_intlen(n);
+	res = malloc(len + 1);
+	if (!res)
+		return (NULL);
+	if (n >= 0)
+		ft_itoa_rec(n, res, len, 0);
+	else
+	{
+		res[0] = '-';
+		ft_itoa_rec(-n, res + 1, len - 1, 0);
+	}
+	res[len] = '\0';
+	return (res);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+
+	i = 0;
+	printf("#%zu: %d\n", ft_intlen(i), i);
+	i = INT_MIN;
+	printf("#%zu: %d\n", ft_intlen(i), i);
+	i = INT_MAX;
+	printf("#%zu: %d\n", ft_intlen(i), i);
+	i = 12345;
+	printf("#%zu: %d\n", ft_intlen(i), i);
+	i = -12345;
+	printf("#%zu: %d\n", ft_intlen(i), i);
+	i = 5;
+	printf("#%zu: %d\n", ft_intlen(i), i);
+}
+*/
