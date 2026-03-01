a5fb16ee8efc360b4c0d19c891b5fb267428c307
ohaponiuk <64164850+ohaponiuk@users.noreply.github.com>
2026-03-01 10:53:49 +0000

    Strlcat: add a helper strnlen

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,26 +6,33 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 23:30:45 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/03/01 10:33:46 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/03/01 10:53:05 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+
+static size_t	ft_strnlen(const char *s, size_t maxlen)
+{
+	const char	*p;
+
+	p = s;
+	while (maxlen-- && *p)
+		p++;
+	return (p - s);
+}
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	char		*d;
 	const char	*s;
 	size_t		dlen;
-	size_t		dsize;
 
 	d = dst;
 	s = src;
-	dsize = dstsize;
-	while (dsize-- && *d)
-		d++;
-	dlen = d - dst;
+	dlen = ft_strnlen(d, dstsize);
 	if (dlen == dstsize)
 		return (dstsize + ft_strlen(src));
+	d += dlen;
 	dstsize -= dlen;
 	while (dstsize-- > 1 && *s)
 		*d++ = *s++;
