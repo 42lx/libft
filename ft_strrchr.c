0138fdbac2f27715e8293c6344161e511cff8433
ohaponiuk <64164850+ohaponiuk@users.noreply.github.com>
2026-03-02 00:22:18 +0000

    strrchr: remove tests

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 00:18:10 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/02/28 20:29:21 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/03/02 00:22:07 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -28,14 +28,3 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (NULL);
 }
-
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	s[] = "tripouille";
-
-	printf("%p\n", ft_strrchr(s, 'z'));
-}
-*/
