af2abcd939caf870843d24a23a7edcc2d2119bf8
Alex (Oleksii) Haponiuk <git@alexh.dev>
2026-03-15 15:51:59 +0000

    strnstr: refactor

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,35 +6,38 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 11:13:00 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/03/01 10:25:54 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/03/15 15:51:18 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
-	const char	*h;
-	const char	*p;
-	size_t		nlen;
+	size_t	nlen;
 
 	nlen = ft_strlen(needle);
 	if (nlen == 0)
 		return ((char *)haystack);
-	if (nlen > n)
-		return (NULL);
-	n -= (nlen - 1);
-	while (n-- && *haystack)
+	while (n-- >= nlen && *haystack)
 	{
-		h = haystack;
-		p = needle;
-		while (*h && *h == *p)
-		{
-			h++;
-			p++;
-		}
-		if (*p == '\0')
+		if (!ft_strncmp(haystack, needle, nlen))
 			return ((char *)haystack);
 		haystack++;
 	}
 	return (NULL);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*bg;
+	char	*ll;
+	int		len;
+
+	bg = "mapman";
+	ll = "apm";
+	len = 3;
+	printf("%s\n", ft_strnstr(bg, ll, len));
+}
+*/
