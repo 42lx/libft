6adb0d40d503ac154f5d636baa1ab50629e3404d
Alex (Oleksii) Haponiuk <git@alexh.dev>
2026-03-15 13:52:33 +0000

    memcmp: remove unneeded zero check

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 00:50:06 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/02/28 20:40:03 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/03/15 13:52:20 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,8 +16,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char	*p1;
 	const unsigned char	*p2;
 
-	if (n == 0)
-		return (0);
 	p1 = s1;
 	p2 = s2;
 	while (n--)
