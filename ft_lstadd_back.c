31ff7d96aada1f5f7730b0b67637a919274efaed
Alex (Oleksii) Haponiuk <git@alexh.dev>
2026-03-17 13:36:07 +0000

    lstadd_back: refactor

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,18 +6,15 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 13:05:37 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/03/02 13:15:20 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/03/17 13:24:43 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*last;
-
-	last = ft_lstlast(*lst);
-	if (!last)
+	if (!*lst)
 		*lst = new;
 	else
-		last->next = new;
+		ft_lstlast(*lst)->next = new;
 }
