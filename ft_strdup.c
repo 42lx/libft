3cc90effe3fe4466ba9a2f59ec98e1af7607a280
Alex (Oleksii) Haponiuk <git@alexh.dev>
2026-03-30 00:14:50 +0100

    strdup: use memcpy for efficiency

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ohaponiu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:33:38 by ohaponiu          #+#    #+#             */
-/*   Updated: 2026/03/18 13:08:59 by ohaponiu         ###   ########.fr       */
+/*   Updated: 2026/03/30 00:14:30 by ohaponiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -20,6 +20,6 @@ char	*ft_strdup(const char *s)
 	res = malloc(len + 1);
 	if (!res)
 		return (NULL);
-	ft_strlcpy(res, s, len + 1);
+	ft_memcpy(res, s, len + 1);
 	return (res);
 }
